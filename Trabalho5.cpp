017a2232f48c3291f33a8335291e4b39a788f6b6
Cassiano Zucco <Cassio.zucco@hotmail.com>
2017-11-09 00:41:55 -0200

    Mudado o vertexShader e CPP para trabalhar com os novos objetos

--- Trabalho5.cpp
+++ Trabalho5.cpp
@@ -24,56 +24,63 @@ int widthWindow, heightWindow, selecionamm, selecionahulk, selecionaanime;
 const float mp = 3.1415926/180.0;
 const float uqp = 3.1415926/90.0;
 
-float XT=0,YT=0,GRAUS=0,XE=1,YE=1,TANXS=0, TANYS=0, E=1, XTM=0,YTM=0,GRAUSM=0,XEM=1,YEM=1,TANXSM=0, TANYSM=0, EM=1.0;
-GLuint colorbuffer, vertexbuffer, MatrixIDXT, MatrixIDYT, MatrixIDGRAUS, MatrixIDXE, MatrixIDYE, MatrixIDTANXS, MatrixIDTANYS, MatrixIDE, MatrixIDXTM, 
-MatrixIDYTM, MatrixIDGRAUSM, MatrixIDXEM, MatrixIDYEM, MatrixIDTANXSM, MatrixIDTANYSM, MatrixIDEM,programID, intPerson;
+float escalaAsteroide1=1,escalaAsteroide2=1,escalaAsteroide3=1,escalaAsteroide4=1,xTranslacaoNave=0,yTranslacaoNave=0;
+GLuint colorbuffer, vertexbuffer, MatrixIDescalaAsteroide1,MatrixIDescalaAsteroide2,MatrixIDescalaAsteroide3,MatrixIDescalaAsteroide4, 
+MatrixIDxTranslacaoNave,MatrixIDyTranslacaoNave ,programID, Object;
 char * isheary = strdup ( "Para Y: Shift + Seta pra Baixo (Diminui), Shift + Seta pra Cima (Cresce)" ) ;
-unsigned char cubeColor[] = { 255, 0, 0, 128 }; // Model color (32bits RGBA)
 
+void TW_CALL RunCB(void *)
+{ 
+  TwTerminate();
+}
+
+void adicionaBarras(){
+	// Initialize the GUI
+	TwInit(TW_OPENGL_CORE, NULL);
+	TwWindowSize(WIDTH, HEIGHT-50);//Alterar area que cobre o mouse
+	
+	TwBar * bar = TwNewBar("Space_Invaders");
+	TwDefine("Space_Invaders position='0 0'  size='1366 768' refresh=0.5");
+	TwDefine(" Space_Invaders color='0 0 0' text=white ");
+	TwDefine(" Space_Invaders alpha=255 "); 
+    TwAddVarRW(bar, "Select MM", TW_TYPE_BOOL8 , &selecionamm, NULL);
+    TwAddVarRW(bar, "Select HULK", TW_TYPE_BOOL8 , &selecionahulk, NULL);
+    TwDefine(" Space_Invaders help='Para reiniciar o a saga precione R' ");
+	TwAddVarRW(bar, "Shear Y:", TW_TYPE_CDSTRING , &isheary, NULL);
+	TwAddButton(bar, " run ", RunCB, NULL, " Label='Start Game' ");
+}
 
 void shaderLoadCreat(){
 	//Leitura e compilação dos Shaders em tempo de execução
 	programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
-	intPerson = glGetUniformLocation(programID, "Person");// PASSA A LOCALIZACAO DA VARIAVEL DA ESCOLHA DO PERSONAGEM PRO VERTEX SHADER
-	MatrixIDXTM = glGetUniformLocation(programID, "XTM");
-	MatrixIDYTM = glGetUniformLocation(programID, "YTM");
-	MatrixIDGRAUSM = glGetUniformLocation(programID, "GRAUSM");
-	MatrixIDXEM = glGetUniformLocation(programID, "XEM");
-	MatrixIDYEM = glGetUniformLocation(programID, "YEM");
-	MatrixIDTANXSM = glGetUniformLocation(programID, "TANXSM");
-	MatrixIDTANYSM = glGetUniformLocation(programID, "TANYSM");
-	MatrixIDEM = glGetUniformLocation(programID, "EM");
-	MatrixIDXT = glGetUniformLocation(programID, "XT");
-	MatrixIDYT = glGetUniformLocation(programID, "YT");
-	MatrixIDGRAUS = glGetUniformLocation(programID, "GRAUS");
-	MatrixIDXE = glGetUniformLocation(programID, "XE");
-	MatrixIDYE = glGetUniformLocation(programID, "YE");
-	MatrixIDTANXS = glGetUniformLocation(programID, "TANXS");
-	MatrixIDTANYS = glGetUniformLocation(programID, "TANYS");
-	MatrixIDE = glGetUniformLocation(programID, "E");
-
+	Object = glGetUniformLocation(programID, "Object");// PASSA A LOCALIZACAO DA VARIAVEL DA ESCOLHA DO PERSONAGEM PRO VERTEX SHADER
+	MatrixIDescalaAsteroide1 = glGetUniformLocation(programID, "escalaAsteroide1");
+	MatrixIDescalaAsteroide2 = glGetUniformLocation(programID, "escalaAsteroide2");
+	MatrixIDescalaAsteroide3 = glGetUniformLocation(programID, "escalaAsteroide3");
+	MatrixIDescalaAsteroide4 = glGetUniformLocation(programID, "escalaAsteroide4");
+	MatrixIDxTranslacaoNave = glGetUniformLocation(programID, "xTranslacaoNave");
+	MatrixIDyTranslacaoNave = glGetUniformLocation(programID, "yTranslacaoNave");
 }
 
-void sendTransfMM(){
-	glUniform1f(MatrixIDXTM, XTM);
-	glUniform1f(MatrixIDYTM, YTM);
-	glUniform1f(MatrixIDGRAUSM, GRAUSM*mp);
-	glUniform1f(MatrixIDEM, EM);
-	glUniform1f(MatrixIDTANXSM, TANXSM*uqp);
-	glUniform1f(MatrixIDTANYSM, TANYSM*uqp);
-	glUniform1f(MatrixIDXEM, XEM);
-	glUniform1f(MatrixIDYEM, YEM);
+void sendTransfNaveValdir(){
+	glUniform1f(MatrixIDxTranslacaoNave, xTranslacaoNave);
+	glUniform1f(MatrixIDyTranslacaoNave, yTranslacaoNave);
 }
 
-void sendTransfHulk(){
-	glUniform1f(MatrixIDXT, XT);
-	glUniform1f(MatrixIDYT, YT);
-	glUniform1f(MatrixIDGRAUS, GRAUS*mp);
-	glUniform1f(MatrixIDE, E);
-	glUniform1f(MatrixIDTANXS, TANXS*uqp);
-	glUniform1f(MatrixIDTANYS, TANYS*uqp);
-	glUniform1f(MatrixIDXE, XE);
-	glUniform1f(MatrixIDYE, YE);
+void sendTransfAsteroide1(){
+	glUniform1f(MatrixIDescalaAsteroide1, escalaAsteroide1);	
+}
+
+void sendTransfAsteroide2(){
+	glUniform1f(MatrixIDescalaAsteroide2, escalaAsteroide2);	
+}
+
+void sendTransfAsteroide3(){
+	glUniform1f(MatrixIDescalaAsteroide3, escalaAsteroide3);	
+}
+
+void sendTransfAsteroide4(){
+	glUniform1f(MatrixIDescalaAsteroide4, escalaAsteroide4);	
 }
 
 void MouseKeyboardMovimentObject(){
@@ -83,8 +90,8 @@ void MouseKeyboardMovimentObject(){
 	double vertical = double(heightWindow - yposMouse * 2)/double(heightWindow);
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
 	{	
-		XTM = horizontal;
-		YTM = vertical;
+		xTranslacaoNave = horizontal;
+		yTranslacaoNave = vertical;
 	}
 
 	}
@@ -92,7 +99,7 @@ void MouseKeyboardMovimentObject(){
 glm::vec4 limitesBoundingBox(std::vector<glm::vec2> objeto){
 	glm::vec4 maiormenor = glm::vec4(objeto[0].x, objeto[0].y, objeto[0].x, objeto[0].y);
 
-	for(int i=1;i < objeto.size();i++){
+	for(int i=1 ;i < objeto.size() ;i++){
 		if(objeto[i].x > maiormenor.x){
 			maiormenor.x = objeto[i].x; //maiormenor[maiorX][maiorY][menorX][MenorY] 
 		}if(objeto[i].y > maiormenor.y){
@@ -103,29 +110,7 @@ glm::vec4 limitesBoundingBox(std::vector<glm::vec2> objeto){
 			maiormenor.w = objeto[i].y; //maiormenor[maiorX][maiorY][menorX][MenorY]
 		}
 	}
-}
-
-void TW_CALL StartGame(void *)
-{ 
-	TwTerminate();
-}
-void adicionaBarras(){
-	// Initialize the GUI
-	TwInit(TW_OPENGL_CORE, NULL);
-	TwWindowSize(WIDTH, HEIGHT);//Alterar area que cobre o mouse
-	
-	TwBar * bar = TwNewBar("Space_Invaders");
-	TwDefine("Space_Invaders position='0 0'  size='1366 768' refresh=0.5");
-	TwDefine(" Space_Invaders color='0 0 0' text=white ");
-	TwDefine(" Space_Invaders alpha=255 "); 
-	TwDefine(" GLOBAL fontsize=3 ");
-	 // Add 'cubeColor' to 'bar': it is a modifable variable of type TW_TYPE_COLOR32 (32 bits color) with alpha
-	TwAddVarRW(bar, "cubeColor", TW_TYPE_COLOR32, &cubeColor," label='Cube color' alpha help='Color and transparency of the cube.' ");
-    TwAddVarRW(bar, "Select MM", TW_TYPE_BOOL8 , &selecionamm, NULL);
-    TwAddVarRW(bar, "Select HULK", TW_TYPE_BOOL8 , &selecionahulk, NULL);
-    TwDefine(" Space_Invaders help='Para reiniciar o a saga precione R' ");
-	TwAddVarRW(bar, "Shear Y:", TW_TYPE_CDSTRING , &isheary, NULL);
-	TwAddButton(bar, " startgame ", StartGame, NULL, " Label='Start Space Invaders'");
+	return maiormenor;
 }
 
 int colisaoEntreCaixas(glm::vec4 a, glm::vec4 b){
@@ -193,7 +178,6 @@ std::vector<glm::vec2> loadModel(const char *path){
 		return vertices;
 	}
 
-
 	while(1){
 		glm::vec2 vertex;
 		int res = fscanf(file, "%f %f\n", &vertex.x,&vertex.y);
@@ -219,14 +203,26 @@ std::vector<glm::vec3> colorir(int tam, float R, float G, float B){
 	return vertexColor;
 }
 
-void drawModel(int selecionaPersonagem, GLenum type, std::vector<glm::vec2> vertices, GLfloat R, GLfloat G, GLfloat B){
-		if(selecionaPersonagem == 1){
-			glUniform1i(intPerson, 1);
-			sendTransfMM();
+void drawModel(int selecionaObject, GLenum type, std::vector<glm::vec2> vertices, GLfloat R, GLfloat G, GLfloat B){
+		if(selecionaObject == 0){
+			glUniform1i(Object, 0);
+			sendTransfNaveValdir();
 		}
-		if(selecionaPersonagem == 2){ 	
-			glUniform1i(intPerson, 2);	
-			sendTransfHulk();
+		else if(selecionaObject == 1){ 	
+			glUniform1i(Object, 1);	
+			sendTransfAsteroide1();
+		}
+		else if(selecionaObject == 2){ 	
+			glUniform1i(Object, 2);	
+			sendTransfAsteroide2();
+		}
+		else if(selecionaObject == 3){ 	
+			glUniform1i(Object, 3);	
+			sendTransfAsteroide3();
+		}
+		else if(selecionaObject == 4){ 	
+			glUniform1i(Object, 4);	
+			sendTransfAsteroide4();
 		}
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	std::vector<glm::vec3> color = colorir(vertices.size(),R,G,B);
@@ -281,10 +277,10 @@ int main( void ){
 	shaderLoadCreat();
 	glGenBuffers(1, &vertexbuffer);
 	glGenBuffers(1, &colorbuffer);
-	std::vector<glm::vec2> asteroide0 = loadModel("./coordinates/asteroide0");
 	std::vector<glm::vec2> asteroide1 = loadModel("./coordinates/asteroide1");
 	std::vector<glm::vec2> asteroide2 = loadModel("./coordinates/asteroide2");
-	std::vector<glm::vec2> asteroide3 = loadModel("./coordinates/asteroide3"); 
+	std::vector<glm::vec2> asteroide3 = loadModel("./coordinates/asteroide3");
+	std::vector<glm::vec2> asteroide4 = loadModel("./coordinates/asteroide4"); 
 	std::vector<glm::vec2> nave_valdir = loadModel("./coordinates/nave_valdir");
 	initText2D( "Holstein.DDS" );
 	do{	
@@ -294,11 +290,11 @@ int main( void ){
 		glUseProgram(programID);
 		// Primeiro buffer de atributo: vértices
 		configLayout(vertexbuffer, colorbuffer);
-		drawModel(1, GL_TRIANGLES, asteroide0, 0.0, 1.0, 0.0);
 		drawModel(1, GL_TRIANGLES, asteroide1, 0.0, 1.0, 0.0);
-		drawModel(1, GL_TRIANGLES, asteroide2, 0.0, 1.0, 0.0);
-		drawModel(1, GL_TRIANGLES, asteroide3, 0.0, 1.0, 0.0);
-		drawModel(2, GL_TRIANGLES, nave_valdir, 0.0, 1.0, 0.0);
+		drawModel(2, GL_TRIANGLES, asteroide2, 0.0, 1.0, 0.0);
+		drawModel(3, GL_TRIANGLES, asteroide3, 0.0, 1.0, 0.0);
+		drawModel(4, GL_TRIANGLES, asteroide4, 0.0, 1.0, 0.0);
+		drawModel(0, GL_TRIANGLES, nave_valdir, 0.0, 1.0, 0.0);
 		MouseKeyboardMovimentObject();
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
